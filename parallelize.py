70238ae47ca7ebb076022beb328686195b18e6c9
Cezary Przemysław Moczulski <c.moczulski@stud.elka.pw.edu.pl>
2022-06-10 12:40:46 +0200

    Rust function

--- parallelize.py
+++ parallelize.py
@@ -2,6 +2,6 @@ import ctypes
 
 parallelized = ctypes.CDLL('target/release/dynamic_lib')
 
-string = ctypes.c_char_p(b"1wse4df123")
-print(parallelized.parallelized_function(string))
-print(string.value)
+args = (ctypes.c_char_p * 2)(*[b'data/4', b'Semantics'])
+argc = ctypes.c_int(len(args))
+print(parallelized.parallelized_function(args, argc))
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,39 @@
 use libc;
 use std::ffi::CStr;
+use std::fs;
 
 # [no_mangle]
-pub extern fn parallelized_function(arg: *mut libc::c_char) -> isize {
-    let argument;
-    unsafe { 
-        match CStr::from_ptr(arg).to_str() {
-            Ok(val) => argument = val.to_owned(),
-            Err(_) => return -1 // arg had invalid UTF-8 data, contents of Err has the details, you may want to change the way this situation is handled
+pub extern fn parallelized_function(args: *const *const libc::c_char, argc: libc::c_uint) -> libc::c_int {
+    let mut arguments = Vec::<String>::new();
+    let mut current_pointer = args;
+    unsafe {
+        for _ in 0..argc {
+            match CStr::from_ptr(*current_pointer).to_str() {
+                Ok(val) => arguments.push(val.to_owned()),
+                Err(_) => return -1 // arg had invalid UTF-8 data, contents of Err has the details, you may want to change the way this situation is handled
+            }
+            current_pointer = current_pointer.offset(1);
         }
     }
-    argument.len().try_into().unwrap()
+    /*
+    Parsing specific arguments to types needed by function
+
+
+
+
+    */
+    count_word_occurences_in_file(&arguments[0], &arguments[1]).try_into().unwrap()
+}
+
+fn count_word_occurences_in_file(filepath: &String, word: &String) -> isize {
+    let contents;
+    match fs::read_to_string(filepath) {
+        Ok(val) => contents = val,
+        Err(err) => {
+            let message = format!("Could not read from file: {}. ", filepath) + &err.to_string();
+            println!("{}", message);
+            return -2;
+        }
+    }
+    contents.matches(word).count().try_into().unwrap()
 }
